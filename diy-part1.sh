e42826494e7d3e8562e687dd0acc8e7101c58d22
Coupile <1172464137@qq.com>
2020-10-20 22:32:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,6 +10,9 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-git clone -b LUCI-LUA-UCITRACK https://github.com/CCnut/feed-netkeeper package/feed-netkeeper
-wget https://raw.githubusercontent.com/r1172464137/r2s-build/main/1.patch 
+#git clone -b LUCI-LUA-UCITRACK https://github.com/CCnut/feed-netkeeper package/feed-netkeeper
+#wget https://raw.githubusercontent.com/r1172464137/r2s-build/main/1.patch 
+git clone https://github.com/mchome/openwrt-dogcom package/openwrt-dogcom
+git clone https://github.com/mchome/luci-app-dogcom package/luci-app-dogcom
+
 
